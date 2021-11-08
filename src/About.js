09669884eacf7416586ca653bfb93282653a81aa
Andre Co <andrecyrilco@hotmail.com>
2021-11-08 04:04:16 -0700

    Edited About page

--- src/About.js
+++ src/About.js
@@ -19,6 +19,28 @@ export default function About() {
                     <ul> 
                         <li><a href="https://github.com/AndreCo17/WEB-3-Assignment-1">Github Link</a></li>
                     </ul>
+                    <ul> Technology Used
+                        <li>REACT</li>
+                        <li>Google</li>
+                        <li>VS Code</li>
+                        <li>JSX</li>
+                        <li>CSS</li>
+                        <li>HTML</li>
+                        <li>React-dom</li>
+                        <li>React-dom-router</li>
+                        <li>React-modal</li>
+                    </ul>
+                    <ul>Sources Used:
+                        <ul>YouTube
+                            <li><a href="https://www.youtube.com/watch?v=w7ejDZ8SWv8">React JS Crash Course 2021</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=mZvKPtH9Fzo">Search Filter React Tutorial - Search Bar in React
+</a></li>
+                            <li></li>
+                        </ul>
+                        <ul>
+                            <li>Stack Overflow</li>
+                        </ul>
+                    </ul>
                 </p>
                 <div>
                     <button className="close-about-button" onClick={() => setModalIsOpen(false)}>Close</button>
